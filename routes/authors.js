832721323cc8fddb29c326a18b142778b4032734
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-02-25 22:47:12 +0000

    Create API for Bears-Team-19 authors (replaces the generated users API)

---
+++ routes/authors.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET authors listing. */
+router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+
+// demo code
+  res.json([{
+    id: 1,
+    name: "guiprix"
+  }, {
+    id: 2,
+    name: "sfiquet"
+  }, {
+    id: 3,
+    name: "ucanfil"
+  }]);
+});
+
+module.exports = router;
