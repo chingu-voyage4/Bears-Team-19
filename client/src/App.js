ac7655bba5d29a71afc8575894d4c60cac27a21e
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-02-25 22:49:27 +0000

    Use the authors API in the React client

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // demo
+  state = {authors: []}
+
+  componentDidMount() {
+    fetch('/api/authors')
+      .then(res => res.json())
+      .then(authors => this.setState({ authors }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +22,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <br></br>
+        <h2>Bears-Team-19</h2>
+        <p>The team, straight from the API:</p>
+        <ul className="Team-list">
+        {this.state.authors.map(author =>
+          <li key={author.id}>{author.name}</li>
+        )}
+        </ul>
       </div>
     );
   }
