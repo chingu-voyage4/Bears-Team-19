04b72b701cfd7983da739ae70ca1238b4c804d48
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-02 08:44:53 +0000

    Create basic Welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -3,33 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  // demo
-  state = {authors: []}
-
-  componentDidMount() {
-    fetch('/api/authors')
-      .then(res => res.json())
-      .then(authors => this.setState({ authors }));
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h1 className="App-title">Projects</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          We connect developers over project ideas
         </p>
-        <br></br>
-        <h2>Bears-Team-19</h2>
-        <p>The team, straight from the API:</p>
-        <ul className="Team-list">
-        {this.state.authors.map(author =>
-          <li key={author.id}>{author.name}</li>
-        )}
-        </ul>
+        <p>
+          This is for developers who:
+          <ul>
+            <li>
+              want to collaborate with other developers on open-source projects
+            </li>
+            <li>
+              want to practice their skills on a real project but don't know what to build
+            </li>
+            <li>
+              need help from other developers to build their project
+            </li>
+          </ul>
+        </p>
+
       </div>
     );
   }
