2f2abf77e1c6c8b695fedd3c6301547d618cbdab
guiprix <guido.vndr@gmail.com>
2018-03-03 15:18:10 +0100

    API-projects

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var authors = require('./routes/authors');
+var projects = require('./routes/projects');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api/authors', authors);
+app.use('/api/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
