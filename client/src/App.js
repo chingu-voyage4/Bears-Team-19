e2c420f7d7f9c06e3b89debfa8d0b8265c8ec9c2
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-03 17:51:27 +0000

    Make the footer stick to the bottom of the page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Welcome from './Welcome';
 import Footer from './Footer';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="text-center">
+      <div className="App text-center d-flex flex-column">
         <Header />
-        <Welcome />
+
+        <div className="AppPage">
+          <Welcome />
+        </div>
+        
         <Footer />
       </div>
     );
