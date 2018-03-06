d40252c4661bcedc978fbd66c105c49c48466854
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-06 23:52:57 +0000

    Add rel="noopener noreferrer" to GitHub link for security

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className="bg-light text-dark py-3 mt-4">
-        <p className="small m-0">Created by <a href="https://github.com/chingu-voyage4/Bears-Team-19" target="_blank">Bear-Team-19</a> during Chingu Voyage 4</p>
+        <p className="small m-0">Created by <a href="https://github.com/chingu-voyage4/Bears-Team-19" target="_blank" rel="noopener noreferrer">Bear-Team-19</a> during Chingu Voyage 4</p>
       </footer>
     );
   }
