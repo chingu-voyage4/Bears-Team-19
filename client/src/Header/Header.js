cd306a472952a1ad1d795a38d576542c8430fda9
Jamie <jamieplayer@gmail.com>
2018-03-06 21:13:15 +0000

    added scss and moved headernavbar to own folder

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import HeaderNavbar from './HeaderNavbar';
+import HeaderNavbar from './Navbar/HeaderNavbar';
 
 class Header extends Component {
   render() {
