15a558c21a4776e07f9154063933485b4961840e
Jamie <jamieplayer@gmail.com>
2018-03-09 15:10:54 +0000

    created logic and styling

---
+++ client/src/Auth/Register/register.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import avatar from './avatar.png';
+import './register.css';
+
+const Register = (props) => {
+    console.log(props, 'this is props');
+    return (
+        <div className="container-fluid d-flex flex-column my-auto">
+                <div className="d-flex flex-column col-lg-5 col-md-3 col-sm-8 mx-auto justify-content-center register-wrapper">
+                    <div className="align-center pt-2 mb-3">
+                        <h2 className="register-text">Register</h2>
+                    </div>
+                    <img src={avatar} className="align-self-center my-3" width="120px" />
+                    <label>Username</label>
+                    <input type="text" placeholder="Username" className="form-control mb-2" value={props.inputs.username} onChange={(e) => {props.handleChange('username', e)} } />
+                    <label>Password</label>
+                    <input type="text" placeholder="Password" className="form-control mb-2" value={props.inputs.password} onChange={(e) => {props.handleChange('password', e)} }/>
+                    <label>Confirm Password</label>
+                    <input type="text" placeholder="Confirm Password" className="form-control mb-2" value={props.inputs.confirmPassword} onChange={(e) => {props.handleChange('confirmPassword', e)} }/>
+                    <button className="btn btn-outline-primary">Submit</button>
+                </div>
+        </div>
+    )
+};
+
+export default Register;
---
+++ client/src/Auth/authContainer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Register from './Register/register.js';
+import axios from 'axios';
+
+const defaultState = {
+    username: '',
+    password: '',
+    passwordMatch: ''
+};
+
+class Auth extends Component {
+    constructor (props) {
+        super();
+         
+        this.state = {
+            username: '',
+            password: '',
+            passwordMatch: ''
+        }
+    }
+
+    clearState = () => {
+        this.setState(defaultState);
+    }
+
+    handleChange = (input, e) => {
+        this.setState({
+            [input]: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+
+        axios.post('/api/register')
+            .then(res => {
+                this.clearState();
+                return res;
+            })
+    }
+
+    render () {
+        return(
+            <Register handleChange={this.handleChange} inputs={this.state} />
+        )
+    }
+};
+
+export default Auth;
