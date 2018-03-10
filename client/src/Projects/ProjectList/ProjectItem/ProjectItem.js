50810768ef0c10b3b26bb5ba78b763f89121bbac
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-10 18:07:56 +0000

    Refactor: tidy up ProjectItem.js

--- client/src/Projects/ProjectList/ProjectItem/ProjectItem.js
+++ client/src/Projects/ProjectList/ProjectItem/ProjectItem.js
@@ -3,49 +3,38 @@ import { Card, CardHeader, CardText, CardBody,
   CardTitle, CardFooter } from 'reactstrap';
 import KeywordList from './KeywordList/KeywordList';
 
-const ValidProject = (props) => {
-  function formatDate(jsonDate){
-    let date = new Date(jsonDate);
-    return date.toLocaleDateString();
-  }
-  return (
-    <Card className="Project">
-      <CardHeader>
-        <CardTitle>{props.project.title ? props.project.title : 'Untitled'}</CardTitle>
-        <KeywordList keywords={props.project.keywords} />
-      </CardHeader>
-      <CardBody>
-        <CardText className="ProjectDescription">{props.project.description}</CardText>
-      </CardBody>
-      <CardFooter className="d-flex flex-row justify-content-between">
-        <div className="ProjectOwner">{props.project.author}</div>
-        <div className="ProjectDateSaved">
-          {props.project.lastSaved? formatDate(props.project.lastSaved) : ''}
-        </div>
-      </CardFooter>
-    </Card>
-  );
-};
-
-const NoProject = () => {
-  return (
-    <Card>
-      <CardBody>No data</CardBody>
-    </Card>
-  );
-};
+function formatDate(jsonDate){
+  let date = new Date(jsonDate);
+  return date.toLocaleDateString();
+}
 
 class ProjectItem extends Component {
   render() {
-    /*
-    <strong>  id : </strong>  {this.props.project.id}- <strong>title : </strong> {this.props.project.title} -
-    <strong>category :</strong> {this.props.project.category} -<strong>descript.:</strong>
-    {this.props.project.description}
-    */
     if (this.props.project) {
-      return <ValidProject project={this.props.project}/>         
+      return (
+        <Card className="Project">
+          <CardHeader>
+            <CardTitle>{this.props.project.title ? this.props.project.title : 'Untitled'}</CardTitle>
+            <KeywordList keywords={this.props.project.keywords} />
+          </CardHeader>
+          <CardBody>
+            <CardText className="ProjectDescription">{this.props.project.description}</CardText>
+          </CardBody>
+          <CardFooter className="d-flex flex-row justify-content-between">
+            <div className="ProjectOwner">{this.props.project.author}</div>
+            <div className="ProjectDateSaved">
+              {this.props.project.lastSaved? formatDate(this.props.project.lastSaved) : ''}
+            </div>
+          </CardFooter>
+        </Card>
+      );
+
     } else {
-      return <NoProject />
+      return (
+        <Card>
+          <CardBody>No data</CardBody>
+        </Card>
+      );
     }
   }
 }
