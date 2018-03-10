3ce5808fd60f46fe0909d6f29de3ee140288fe76
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-10 10:03:04 +0000

    Display projects in cards showing title and keywords

--- client/src/Projects/ProjectList/ProjectItem/ProjectItem.js
+++ client/src/Projects/ProjectList/ProjectItem/ProjectItem.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
+import { Card, CardHeader, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+const KeywordList = (props) => {
+  // if there are no keywords, don't add anything to the HTML
+  if (!props.keywords || props.keywords.length == 0){
+    return null;
+  }
+
+  let keywords = props.keywords.map((keyword, id) => {
+    return (
+        <li key={id} className="ProjectKeyword border rounded px-2 m-1">{keyword}</li>
+      );
+  });
+
+  return (
+    <ul className="ProjectKeywordList list-unstyled d-flex justify-content-center mb-0">
+      {keywords}
+    </ul>  
+  );    
+};
+
+const ValidProject = (props) => {
+  return (
+    <Card className="Project">
+      <CardHeader>
+        <CardTitle>{props.project.title ? props.project.title : 'Untitled'}</CardTitle>
+        <KeywordList keywords={props.project.keywords} />
+      </CardHeader>
+      <CardBody>
+      </CardBody>
+    </Card>
+  );
+};
+
+const NoProject = () => {
+  return (
+    <Card>
+      <CardBody>No data</CardBody>
+    </Card>
+  );
+};
 
 class ProjectItem extends Component {
   render() {
-    return (
-      <li className="Project">
-        <strong>  id : </strong>  {this.props.project.id}- <strong>title :</strong>{this.props.project.title} -
-        <strong>category :</strong> {this.props.project.category} -<strong>descript.:</strong>
-        {this.props.project.description}
-      </li>
-    );
+    /*
+    <strong>  id : </strong>  {this.props.project.id}- <strong>title : </strong> {this.props.project.title} -
+    <strong>category :</strong> {this.props.project.category} -<strong>descript.:</strong>
+    {this.props.project.description}
+    */
+    if (this.props.project) {
+      return <ValidProject project={this.props.project}/>         
+    } else {
+      return <NoProject />
+    }
   }
 }
 
---
+++ client/src/Projects/ProjectList/ProjectItem/ProjectItem.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import ProjectItem from './ProjectItem';
+import ErrorBoundary from '../../../Utils/ErrorBoundary/ErrorBoundary';
+
+describe('ProjectItem component', () => {
+  // no project
+  it('should display "No data" if it is not given a project object', () => {
+    let project;
+    const wrapper = mount(
+          <ProjectItem />
+    );
+    expect(wrapper.text()).toContain("No data");
+  });
+
+  // Title
+  it('should display the project\'s title', () => {
+    let project = { title: 'Example title' };
+    const wrapper = mount(
+          <ProjectItem project={project}/>
+    );
+    expect(wrapper.text()).toContain('Example title');
+  });
+
+  it('should display "Untitled" if the project doesn\'t have a title', () => {
+    let project = { id: 34 };
+    const wrapper = mount(
+          <ProjectItem project={project}/>
+    );
+    expect(wrapper.text()).toContain('Untitled');
+  });
+
+  it('should display "Untitled" if the project has an empty title', () => {
+    let project = { id: 34, title: '' };
+    const wrapper = mount(
+          <ProjectItem project={project}/>
+    );
+    expect(wrapper.text()).toContain('Untitled');
+  });
+
+  // Keywords
+  it('should display the list of keywords', () => {
+    let project = { title: 'Title', keywords: ['html', 'css', 'js'] };
+    const wrapper = mount(
+      <ErrorBoundary>
+        <ProjectItem project={project}/>
+      </ErrorBoundary>
+    );
+    expect(wrapper.find('.ProjectKeywordList')).toHaveLength(1);
+    expect(wrapper.find('.ProjectKeyword')).toHaveLength(3);
+  });
+
+  it('should not create HTML elements for keywords if there is no keyword list', () => {
+    let project = { title: 'Title' };
+    const wrapper = mount(
+      <ErrorBoundary>
+        <ProjectItem project={project}/>
+      </ErrorBoundary>
+    );
+    expect(wrapper.find('.ProjectKeywordList')).toHaveLength(0);
+    expect(wrapper.find('.ProjectKeyword')).toHaveLength(0);
+  });
+
+  it('should not create HTML elements for keywords if the keyword list is empty', () => {
+    let project = { title: 'Title', keywords: [] };
+    const wrapper = mount(
+      <ErrorBoundary>
+        <ProjectItem project={project}/>
+      </ErrorBoundary>
+    );
+    expect(wrapper.find('.ProjectKeywordList')).toHaveLength(0);
+    expect(wrapper.find('.ProjectKeyword')).toHaveLength(0);
+  });
+
+  // Description
+  // Owner
+  // Date created
+  // test the key as well
+});
