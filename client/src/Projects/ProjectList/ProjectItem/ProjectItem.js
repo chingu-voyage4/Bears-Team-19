d23030eb40579001558c8a8185003d92f85cae0c
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-15 19:16:39 +0000

    Refactor ProjectList and ProjectItem into functional components

--- client/src/Projects/ProjectList/ProjectItem/ProjectItem.js
+++ client/src/Projects/ProjectList/ProjectItem/ProjectItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardHeader, CardText, CardBody,
   CardTitle, CardFooter } from 'reactstrap';
 import KeywordList from './KeywordList/KeywordList';
@@ -27,43 +27,41 @@ function formatDate(jsonDate){
   return dateStr;
 }
 
-class ProjectItem extends Component {
-  render() {
-    if (this.props.project) {
-      let descriptionItems;
-      if (this.props.project.description && typeof this.props.project.description === 'string'){
-        descriptionItems = this.props.project.description.split(/\r\n\s*/)
-          .reduce((acc, paragraph) => paragraph !== '' ? acc.concat(paragraph) : acc, [])
-          .map((paragraph, index) => {
-          return <CardText key={index} className="ProjectDescriptionItem">{paragraph}</CardText>;
-        });
-      } else {
-        descriptionItems = '';
-      }
-
-      return (
-        <Card className="Project mt-3 text-left">
-          <CardHeader>
-            <CardTitle>{this.props.project.title ? this.props.project.title : 'Untitled'}</CardTitle>
-            <KeywordList keywords={this.props.project.keywords} />
-          </CardHeader>
-          <CardBody className="ProjectDescription">{descriptionItems}</CardBody>
-          <CardFooter className="d-flex flex-row justify-content-between text-muted">
-            <div className="ProjectOwner">{this.props.project.author}</div>
-            <div className="ProjectDateSaved">
-              {this.props.project.lastSaved? formatDate(this.props.project.lastSaved) : ''}
-            </div>
-          </CardFooter>
-        </Card>
-      );
-
+const ProjectItem = (props) => {
+  if (props.project) {
+    let descriptionItems;
+    if (props.project.description && typeof props.project.description === 'string'){
+      descriptionItems = props.project.description.split(/\r\n\s*/)
+        .reduce((acc, paragraph) => paragraph !== '' ? acc.concat(paragraph) : acc, [])
+        .map((paragraph, index) => {
+        return <CardText key={index} className="ProjectDescriptionItem">{paragraph}</CardText>;
+      });
     } else {
-      return (
-        <Card>
-          <CardBody>No data</CardBody>
-        </Card>
-      );
+      descriptionItems = '';
     }
+
+    return (
+      <Card className="Project mt-3 text-left">
+        <CardHeader>
+          <CardTitle>{props.project.title ? props.project.title : 'Untitled'}</CardTitle>
+          <KeywordList keywords={props.project.keywords} />
+        </CardHeader>
+        <CardBody className="ProjectDescription">{descriptionItems}</CardBody>
+        <CardFooter className="d-flex flex-row justify-content-between text-muted">
+          <div className="ProjectOwner">{props.project.author}</div>
+          <div className="ProjectDateSaved">
+            {props.project.lastSaved? formatDate(props.project.lastSaved) : ''}
+          </div>
+        </CardFooter>
+      </Card>
+    );
+
+  } else {
+    return (
+      <Card>
+        <CardBody>No data</CardBody>
+      </Card>
+    );
   }
 }
 
--- client/src/Projects/ProjectList/ProjectList.js
+++ client/src/Projects/ProjectList/ProjectList.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ProjectItem from './ProjectItem/ProjectItem';
 
-class Projects extends Component {
-
-  render() {
-    let projectItems;
-    if (this.props.projects && this.props.projects.length > 0){
-      projectItems = this.props.projects.map(project => {
-        return (
-          <ProjectItem key={project.title} project={project}/>
-        );
-      });
-    } else {
-      return(
-          <p className="mt-5">No projects found</p>
-        );
-    }
-
-    return (
-      <div className="ProjectList">
-        {projectItems}
-      </div>
-    );
+const Projects = (props) => {
+  let projectItems;
+  if (props.projects && props.projects.length > 0){
+    projectItems = props.projects.map(project => {
+      return (
+        <ProjectItem key={project.title} project={project}/>
+      );
+    });
+  } else {
+    return(
+        <p className="mt-5">No projects found</p>
+      );
   }
+
+  return (
+    <div className="ProjectList">
+      {projectItems}
+    </div>
+  );
 }
 
 export default Projects;
