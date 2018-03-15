ddffa863c2180c4703916e46594f55519c9d2067
Jamie <jamieplayer@gmail.com>
2018-03-15 22:43:48 +0000

    moved server to own folder and updated npm scripts

---
+++ server/routes/authors.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET authors listing. */
+router.get('/', (req, res, next) => {
+//  res.send('respond with a resource');
+
+// demo code
+  res.json([{
+    id: 1,
+    name: 'guiprix',
+  }, {
+    id: 2,
+    name: 'sfiquet',
+  }, {
+    id: 3,
+    name: 'ucanfil',
+  }]);
+});
+
+module.exports = router;
---
+++ server/routes/projects.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.json([
+    {
+      id: '1',
+      title: 'snap this',
+      author: 'Bears 19',
+      category: ['web', 'development'],
+      keywords: ['web', 'react', 'js'],
+      description: 'lore ipsum....',
+    },
+
+    {
+      id: '1',
+      title: 'buckets challange',
+      author: 'Bears 19',
+      category: ['gaming'],
+      keywords: ['web', 'react', 'js'],
+      description: 'lore ipsum....',
+    },
+
+  ]);
+});
+
+module.exports = router;
