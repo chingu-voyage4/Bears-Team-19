e9936ac9f472bd2d672e704b103aa1b687b10e1b
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-03-15 16:14:40 +0000

    Connect to database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -12,6 +12,26 @@ const authors = require('./routes/authors');
 const projects = require('./routes/projects');
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const url = require('url');
+
+const mongoDB = url.format({
+  protocol: 'mongodb',
+  slashes: true,
+  auth: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  hostname: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  pathname: process.env.DB_PATH,
+});
+console.log('Connecting to', mongoDB);
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,7 +57,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res /* , next */) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
