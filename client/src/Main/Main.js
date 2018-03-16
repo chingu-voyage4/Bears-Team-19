21ee3583349d3bf98518fa5f8476e825f5a65d93
guiprix <guido.vndr@gmail.com>
2018-03-16 17:53:57 +0100

    modified Main.js and Welcome.js to create a new compontent and a new link to the form page

--- client/src/Main/Main.js
+++ client/src/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Welcome from '../Welcome/Welcome';
 import Projects from '../Projects/Projects';
+import ProjectCreate from '../ProjectCreate/ProjectCreate';
 import './Main.css';
 
 const Main = () => (
@@ -9,6 +10,8 @@ const Main = () => (
     <Switch>
       <Route exact path='/' component={Welcome} />
       <Route exact path='/projects' component={Projects} />
+      <Route exact path='/projectCreate' component={ProjectCreate} />
+
     </Switch>
   </main>
 )
--- client/src/Welcome/Welcome.js
+++ client/src/Welcome/Welcome.js
@@ -6,11 +6,11 @@ class Welcome extends Component {
     return (
       <div className="container-fluid text-center py-3 d-flex flex-column my-auto">
         <h1>Projects</h1>
-        
+
         <p className="lead">
           Find your next collaborative project
         </p>
-        
+
         <ul className="list-unstyled my-4">
           <li className="m-3">
             Can't think what to build next? Browse our projects
@@ -25,6 +25,9 @@ class Welcome extends Component {
 
         <div>
           <Link to="/projects" className="btn btn-primary">Browse Projects</Link>
+        </div>
+        <div>
+          <Link to="/projectCreate" className="btn btn-primary">Create Project</Link>
         </div>
       </div>
     );
