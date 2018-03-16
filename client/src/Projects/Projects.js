532f5dfc07207accf7c45b76c6d5ccabfa7f737e
guiprix <guido.vndr@gmail.com>
2018-03-16 18:05:57 +0100

    deleted AddPorject folder and files from old location, delelted code from project.js that is not needed

--- client/src/Projects/Projects.js
+++ client/src/Projects/Projects.js
@@ -15,17 +15,11 @@ class Projects extends Component {
       .then(res => res.json())
       .then(projects => this.setState({ projects: projects }));
   }
-  handleAddProject(project){
-    let projects = this.state.projects;
-    projects.push(project);
-    this.setState({projects:projects});
-    console.log(project);
-  }
 
   render() {
     return (
       <div className="container text-center">
-        <AddProject addProject={this.handleAddProject.bind(this)} />
+        
         <h1>Browse All Projects</h1>
         <ProjectList projects={ this.state.projects } />
       </div>
