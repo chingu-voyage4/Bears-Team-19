7d3d83cc264033e3749b4ad8e9d5875498d5ae77
guiprix <guido.vndr@gmail.com>
2018-03-21 19:28:40 +0100

    fixed sintax/identation, deleted author from form, commented out category from form

--- client/src/ProjectCreate/AddProject.js
+++ client/src/ProjectCreate/AddProject.js
@@ -1,79 +1,75 @@
-
 import React, { Component } from 'react';
 //import ProjectItem from './ProjectItem/ProjectItem';
 
 class AddProject extends Component {
-
-constructor(){
-  super();
-  this.state={
-    newProject: {}
+  constructor() {
+    super();
+    this.state = {
+      newProject: {}
+    };
   }
-}
 
   static defaultProps = {
     categories: ['web design', 'web development', 'mobile development']
-}
-handleSubmit(e){
-  if(this.refs.title.value=== ''){
-    alert('Title is required')
-  }else{
-    this.setState({newProject:{
-      title:this.refs.title.value,
-      author:this.refs.author.value,
-      description:this.refs.description.value,
-      keywords:this.refs.keywords.value,
-      category: this.refs.category.value
-
-    }},function(){
-      this.props.addProject(this.state.newProject);
-      //console.log('porject obj' + this.props);
-    });
-
+  };
+  handleSubmit(e) {
+    if (this.refs.title.value === '') {
+      alert('Title is required');
+    } else {
+      this.setState(
+        {
+          newProject: {
+            title: this.refs.title.value,
+            description: this.refs.description.value,
+            keywords: this.refs.keywords.value
+            //category: this.refs.category.value TODO
+          }
+        },
+        function() {
+          this.props.addProject(this.state.newProject);
+          //console.log('porject obj' + this.props);
+        }
+      );
+    }
+    e.preventDefault();
   }
-  e.preventDefault();
-}
 
   render() {
-    let categoryOptions = this.props.categories.map(category =>{
-      return <option key={category} value={category}>{category}</option>
+    let categoryOptions = this.props.categories.map(category => {
+      return (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      );
     });
 
-
     return (
-      <div >
-        <h1>Add New Project</h1>
-
-        <form onSubmit={this.handleSubmit.bind(this)}>
-        <div>
-          <label>Title</label><br />
-          <input type="text" ref="title" />
-        </div>
-        <div>
-          <label>Author</label><br />
-          <input type="text" ref="author" />
-        </div>
-        <div>
-        <label>Description</label><br />
-        <textarea ref="description" />
-      </div>
       <div>
-      <label>Keywords</label><br />
-      <input type="text" ref="keywords" />
-    </div>
+        <h1>Add New Project</h1>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <label className="d-block">Title</label>
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label className="d-block">Description</label>
+            <textarea ref="description" />
+          </div>
+          <div>
+            <label className="d-block">Keywords</label>
+            <input type="text" ref="keywords" />
+          </div>
 
-        <div>
-          <label>Category</label><br />
-          <select ref="category" > {categoryOptions}</select>
-        </div>
+          {/*} TODO
+          <div>
+            <label>Category</label>
+            <br />
+            <select ref="category"> {categoryOptions}</select>
+          </div>
+          */}
 
-
-
-
-        <input type="submit" value="submit" />
-
+          <input type="submit" value="submit" />
         </form>
-
       </div>
     );
   }
--- client/src/ProjectCreate/ProjectCreate.js
+++ client/src/ProjectCreate/ProjectCreate.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
 import AddProject from './AddProject';
 
-
-
 class ProjectCreate extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = { projects: [] };
   }
 
-
-
-  handleAddProject(project){
+  handleAddProject(project) {
     let projects = this.state.projects;
     projects.push(project);
-    this.setState({projects:projects});
+    this.setState({ projects: projects });
     console.log(project); //see the object added, in console
   }
 
   render() {
     return (
       <div className="container text-center">
-
         <AddProject addProject={this.handleAddProject.bind(this)} />
-
       </div>
-    )
+    );
   }
 }
 
