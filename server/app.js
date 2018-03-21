737b40c0402e0f7196b9a1e2d7ee1783c8ade1ab
Jamie <jamieplayer@gmail.com>
2018-03-21 11:21:23 +0000

    added passport initilization and linting fixes

---
+++ server/app.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+// Routes
+const index = require('./routes/index');
+const projects = require('./routes/projects');
+
+// Passport
+const passport = require('passport');
+require('./components/Auth/Passport.js');
+const auth = require('./components/Auth/Auth.js');
+
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const url = require('url');
+
+const mongoDB = url.format({
+  protocol: 'mongodb',
+  slashes: true,
+  auth: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  hostname: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  pathname: process.env.DB_PATH,
+});
+console.log('Connecting to', mongoDB);
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Authentication
+
+app.use(bodyParser());
+app.use(require('express-session')({ secret: process.env.SESSION_SECRET }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/api/projects', projects);
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res /* , next */) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ server/app.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('./app.js');
+
+describe('Routes return data', () => {
+  test('Get request to route path returns a response', (done) => {
+    request(app).get('/')
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+
+  test('Get JSON response when requesting projects', async () => {
+    const response = await request(app).get('/api/projects');
+    expect(response.type).toEqual('application/json');
+    expect(response.body.length).toEqual(5);
+    expect(response.body).toMatchSnapshot();
+  });
+});
