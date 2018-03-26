deb82bab2d00738c82d5b12cba3ca6809ad61398
Jamie <jamieplayer@gmail.com>
2018-03-26 12:50:04 +0100

    commented out view engine and change static directory to public/build

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res /* , next */) => {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(`${__dirname}/index.html`));
 });
 
 module.exports = router;
