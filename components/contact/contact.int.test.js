20a2de1d65fbdd7e40bcff8c4c31102b5732baf5
Jamie <jamieplayer@gmail.com>
2018-04-13 21:07:03 +0100

    updated tests follow to follow superagent like projects

--- components/contact/contact.int.test.js
+++ components/contact/contact.int.test.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
+const request = require('supertest');
 const nodemailer = require('nodemailer');
+const app = require('../../app.js');
+const User = require('../user/usermodel.js');
+const Project = require('../projects/projectmodel.js');
+
+const agent = request.agent(app);
 
 const transporter = nodemailer.createTransport({
   host: process.env.TEST_MAIL_HOST,
@@ -18,13 +24,86 @@ const mail = {
   html: '<p>Test HTML</p>',
 };
 
+async function authenticatedRequest(done) {
+  const testUser = {
+    username: 'testuser',
+    email: 'test@test.com',
+    password: 'testpass',
+  };
+  const newUser = new User(testUser);
+  await newUser.save()
+    .then(() => {
+      agent.post('/api/auth/login')
+        .send({
+          username: 'testuser',
+          password: 'testpass',
+        })
+        .then(() => done());
+    });
+}
+
 // Email data
 describe('Emails', () => {
-  test.only('Email returns a 250 response code accepted', async () => {
+  beforeAll(async done => authenticatedRequest(done));
+
+  afterAll(async () => {
+    await User.remove({});
+  });
+
+  test('Email returns a 250 response code accepted', async () => {
     const sentMessage = await transporter.sendMail(mail);
-    console.log(sentMessage);
 
     expect(sentMessage.response).toMatch('250');
     expect(sentMessage.envelope.from).toEqual('q2qwv34vgtex34yl@ethereal.email');
+  });
+
+  test('Get contact page returns a 404', async () => {
+    const response = await request(app).get('/api/contact');
+    expect(response.statusCode).toBe(404);
+  });
+  test('Post to contact requires returns 404 without query parameter', async () => {
+    const response = await request(app).post('/api/contact/');
+    expect(response.statusCode).toEqual(404);
+  });
+  test('Post to contact requires user is logged in', async () => {
+    const message = {
+      message: 'test subject',
+      body: 'test message',
+    };
+
+    // object ID doesn't exist but should check login before id
+    const response = await request(app).post('/api/contact/5ad081ed1ffc65330082cc48').send(message);
+    expect(response.statusCode).toEqual(401);
+    expect(response.body).toHaveProperty('message');
+  });
+
+  test('Post to contact requires a subject and description to exist', async () => {
+    // using the persistent session
+    const response = await agent.post('/api/contact/5ad081ed1ffc65330082cc48').send({ subject: '', body: '' });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('Post to contact requires an existing user to contact', async () => {
+    const message = {
+      subject: 'test subject',
+      body: 'test message',
+    };
+    const projectObj = {
+      project: {
+        title: 'test title',
+        description: 'test desc',
+        keywords: ['one'],
+      },
+    };
+
+    const pRes = await agent
+      .post('/api/projects')
+      .send(projectObj)
+      .expect(200);
+
+    const response = await agent.post(`/api/contact/${pRes.body._id}`).send(message);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toEqual('Message Successfuly sent.');
   });
 });
