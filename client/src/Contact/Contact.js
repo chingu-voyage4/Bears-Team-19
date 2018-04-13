fdf1fadc89db18f981e1d5017128f0aeed08d4e8
Jamie <jamieplayer@gmail.com>
2018-04-13 23:13:21 +0100

    added methods to form, created controlled inputs

--- client/src/Contact/Contact.js
+++ client/src/Contact/Contact.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import ContactForm from './ContactForm.js';
+import axios from 'axios';
 
 class Contact extends Component {
 
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            subject: '',
+            body: ''
+        }
+    }
+
+    clearState = () => {
+        return this.setState({
+            subject: '',
+            body: ''
+        });
+    }   
+
+    componentWillMount = (props) => {
+        axios.get(`/api/projects/${this.props.match.projectId}`)
+            .then(res => {
+                return this.setState({
+                    project: res.data
+                })
+            })
+    }
+
+    handleChange = (input, event) => {
+        this.setState({
+            [input]: event.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        axios.post(`/api/contact/${this.state.projectId}`)
+            .then(res => {
+                console.log(res);
+                this.clearState();
+            })
+    }
+
+
     render () {
         return (
-             <ContactForm />
+             <ContactForm 
+                projectTitle={this.state.projecTitle}
+                subject={this.state.subject} 
+                body={this.state.body}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+             />
         )
     }
 }
--- client/src/Contact/ContactForm.js
+++ client/src/Contact/ContactForm.js
@@ -8,20 +8,32 @@ const ContactForm = (props) => {
             <div className="col-xl-5 col-lg-5 col-md-7 col-sm-10 col-xs-11 my-5 mx-auto">
                 <div className="card contact-card">
                     <div class="card-header text-black bg-light text-left">
-                       {props.projectName || "Project Name"}
+                       {props.projectTitle || "Project Name"}
                     </div>
                     <div class="card-body text-left">
                         <form>
                             <div className="form-group">
                                 <label for="subjectLine">Subject</label>
-                                <input  type="text" className="form-control" id="subjectLine" placeholder="Enter Subject"/>
+                                <input  type="text" 
+                                className="form-control" 
+                                id="subjectLine" 
+                                placeholder="Enter Subject"
+                                onChange={(e)=>{ props.handleChange('subject', e)}}
+                                />
                             </div>
                             <div class="form-group">
                                 <label for="exampleTextarea">Message</label>
-                                <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
+                                <textarea className="form-control" 
+                                id="exampleTextarea" 
+                                rows="3"
+                                onChange={(e)=>{ props.handleChange('body', e)}}
+                                ></textarea>
                             </div>
                             <div>
-                                <button type="button" className="btn btn-primary btn-block">Submit</button>
+                                <button type="button" 
+                                className="btn btn-primary btn-block"
+                                onClick={()=> {props.handleSubmit()}}
+                                >Submit</button>
                             </div>
                         </form>
                     </div>
