50a3be9ab03f5dd879984799c1ab8da11302b9e2
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-04-13 11:45:10 +0100

    Fix SetTimeout bug in NewProjectPage with AsyncFormPage

--- client/src/NewProjectPage/AddProjectForm/AddProjectForm.js
+++ client/src/NewProjectPage/AddProjectForm/AddProjectForm.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import LabelledInput from '../../Form/LabelledInput';
 import LabelledTextarea from '../../Form/LabelledTextarea';
 import LabelledTagInput from '../../Form/LabelledTagInput';
@@ -40,7 +41,7 @@ class AddProjectForm extends Component {
   }
 
   handleSubmit(e) {
-    this.props.addProject({
+    this.props.onSubmit({
       title: this.state.title,
       description: this.state.description,
       keywords: this.state.keywords,
@@ -49,41 +50,44 @@ class AddProjectForm extends Component {
 
   render() {
     return (
-      <Fragment>
-        <form>
-          <LabelledInput
-            inputId="title" 
-            label="Title" 
-            inputText={this.state.title} 
-            onChange={this.handleTitleChange} 
-            disabled={this.props.disabled}
-          />
-          <LabelledTextarea
-            inputId="description" 
-            label="Description" 
-            inputText={this.state.description} 
-            onChange={this.handleDescriptionChange} 
-            disabled={this.props.disabled}
-          />
-          <LabelledTagInput
-            inputId="keywords" 
-            label="Keywords" 
-            value={this.state.keywords} 
-            onChange={this.handleKeywordsChange} 
-            disabled={this.props.disabled}
-          />
-          <div className="form-group">
-            <button
-              className="btn btn-primary"
-              type="button"
-              onClick={this.handleSubmit}
-              disabled={this.props.disabled || !this.state.canSubmit}
-            >Submit</button>
-          </div>
-        </form>
-      </Fragment>
+      <form>
+        <LabelledInput
+          inputId="title" 
+          label="Title" 
+          inputText={this.state.title} 
+          onChange={this.handleTitleChange} 
+          disabled={this.props.disabled}
+        />
+        <LabelledTextarea
+          inputId="description" 
+          label="Description" 
+          inputText={this.state.description} 
+          onChange={this.handleDescriptionChange} 
+          disabled={this.props.disabled}
+        />
+        <LabelledTagInput
+          inputId="keywords" 
+          label="Keywords" 
+          value={this.state.keywords} 
+          onChange={this.handleKeywordsChange} 
+          disabled={this.props.disabled}
+        />
+        <div className="form-group">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={this.handleSubmit}
+            disabled={this.props.disabled || !this.state.canSubmit}
+          >Submit</button>
+        </div>
+      </form>
     );
   }
 }
+
+AddProjectForm.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default AddProjectForm;
