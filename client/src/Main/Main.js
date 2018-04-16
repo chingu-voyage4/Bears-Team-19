2ce9dd2df538d6eb4e1b5dbe778fe5f6b2bd8472
Jamie <jamieplayer@gmail.com>
2018-04-16 12:56:39 +0100

    added contact route to Main routes

--- client/src/Main/Main.js
+++ client/src/Main/Main.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from '../Utils/Route/PrivateRoute';
 import Welcome from '../Welcome/Welcome';
 import Projects from '../Projects/Projects';
+import NewProjectPage from '../NewProjectPage/NewProjectPage';
 import Auth from '../Auth/Auth.js';
+import LoginPage from '../LoginPage/LoginPage';
 import Contact from '../Contact/Contact.js';
 import './Main.css';
 
-const Main = () => (
+const Main = (props) => (
   <main className="Main d-flex">
     <Switch>
       <Route exact path='/' component={Welcome} />
       <Route exact path='/projects' component={Projects} />
+      <PrivateRoute exact path='/projects/create' component={NewProjectPage} componentProps={props} />
+      <PrivateRoute path='/contacat/:id' component={Contact} />
       <Route exact path='/register' component={Auth} />
-      <Route path="/contact/:id" component={Contact} />
+      <Route 
+        exact path='/login' 
+        render={(routeProps) => <LoginPage {...props} {...routeProps} />}
+      />
     </Switch>
   </main>
 )
