178420ef9618c76ae5ba90a5b192a2f2afc0ce8c
Jamie <jamieplayer@gmail.com>
2018-04-20 09:18:22 +0100

    new path with routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res /* , next */) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(`${__dirname}/index.html`));
 });
 
 module.exports = router;
