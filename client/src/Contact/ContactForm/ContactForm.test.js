c4782bdbc0af7d67556fa9f2874e9602dcd69f8d
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-04-20 16:59:25 +0100

    Fix proptype warnings in ContactForm.test.js

--- client/src/Contact/ContactForm/ContactForm.test.js
+++ client/src/Contact/ContactForm/ContactForm.test.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ContactForm from './ContactForm.js';
 
+const props = {
+    handleChange: () => {},
+    handleSubmit: () => {},
+    subject: '',
+    body: '',
+};
+
 describe('ContactForm Component', () => {
 
 
     it('Component renders one input & one text area & one button', () => {
         const wrapper = shallow(
-            <ContactForm />
+            <ContactForm {...props}/>
         );
         const input = wrapper.find('input')
         const textArea = wrapper.find('textarea');
@@ -19,28 +26,30 @@ describe('ContactForm Component', () => {
 
     it('displays the name of the project', () => {
         const projectTitle = "Example Project";
-        const wrapper = shallow(<ContactForm projectTitle={projectTitle} />);
+        const wrapper = shallow(<ContactForm {...props} projectTitle={projectTitle} />);
         expect(wrapper.text()).toContain("Example Project");
     })
 
     it('Input has same value as subject prop', () => {
-        const subject = "Test Subject";
-        const wrapper = shallow(<ContactForm subject={subject} />);
+        const newSubject = "Test Subject";
+        const { subject, ...otherProps} = props;
+        const wrapper = shallow(<ContactForm {...otherProps} subject={newSubject} />);
 
         const input = wrapper.find('input');
         expect(input.props().value).toMatch(/Test Subject/);
     });
 
     it('Textarea has same value as body prop', () => {
-        const body = "Test Body";
-        const wrapper = shallow(<ContactForm body={body} />);
+        const newBody = "Test Body";
+        const { body, ...otherProps} = props;
+        const wrapper = shallow(<ContactForm {...otherProps} body={newBody} />);
 
         const Textarea = wrapper.find('textarea');
         expect(Textarea.props().value).toMatch(/Test Body/);
     })
 
     it('Submit button is disabled if disabled is true', () => {
-        const wrapper = shallow(<ContactForm  disabled={true}/>);
+        const wrapper = shallow(<ContactForm {...props} disabled={true}/>);
         const button = wrapper.find('button');
         expect(button.prop('disabled')).toBe(true);
     })
@@ -48,7 +57,7 @@ describe('ContactForm Component', () => {
 
     it('Submit button is enabled if disabled is false', () => {
 
-        const wrapper = shallow(<ContactForm  disabled={false}/>);
+        const wrapper = shallow(<ContactForm {...props} disabled={false}/>);
         const button = wrapper.find('button');
         expect(button.prop('disabled')).toBe(false);
     })
