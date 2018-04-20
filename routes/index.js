95665960a0d29bacdce1d61c140749f1e7332d35
Jamie <jamieplayer@gmail.com>
2018-04-20 11:18:35 +0100

    removed extra index.html from path.join

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res /* , next */) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../') });
+  res.sendFile('index.html', { root: path.join(__dirname, '../public/build') });
 });
 
 module.exports = router;
