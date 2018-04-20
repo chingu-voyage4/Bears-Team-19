72e47c94484f419051476a48c4e5c405275abb5b
Jamie <jamieplayer@gmail.com>
2018-04-20 09:00:19 +0100

    updated route to index file

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res /* , next */) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
+  res.sendFile('index.html');
 });
 
 module.exports = router;
